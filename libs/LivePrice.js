dbe906e76987ad4fbd41a5410336b2ace8dc07d9
HyperGround <112865572+HyperGround@users.noreply.github.com>
2025-07-13 13:47:26 +0300

    Update LivePrice.js

--- libs/LivePrice.js
+++ libs/LivePrice.js
@@ -1,22 +1,45 @@
+// TwelveData API Key
+const apiKey = "3d3bcaf515d34107b07d81ef9ac48af9";
 
 function getPrice(symbol) {
-  var url = "https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=3d3bcaf515d34107b07d81ef9ac48af9";
-  var res = HTTP.get({
-    url: url,
-    success: function(response) {
-      let data = JSON.parse(response.body);
-      if (data && data.price) {
-        return data.price;
-      } else {
-        return "❌ ناتوانرایەوە نرخ بخوێندرێتەوە.";
-      }
-    },
-    error: function(err) {
-      return "❌ هەڵە لەوەی خوێندنی نرخەکە.";
+  var url = "https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=" + apiKey;
+  
+  try {
+    let res = HTTP.get({ url: url });
+    let data = JSON.parse(res.body);
+
+    if (data && data.price) {
+      return parseFloat(data.price);
+    } else {
+      return null;
     }
-  });
-  return res;
+  } catch (err) {
+    return null;
+  }
 }
+
+publish({
+  getPrice: getPrice
+});// TwelveData API Key
+const apiKey = "3d3bcaf515d34107b07d81ef9ac48af9";
+
+function getPrice(symbol) {
+  var url = "https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=" + apiKey;
+  
+  try {
+    let res = HTTP.get({ url: url });
+    let data = JSON.parse(res.body);
+
+    if (data && data.price) {
+      return parseFloat(data.price);
+    } else {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+}
+
 publish({
   getPrice: getPrice
 });
